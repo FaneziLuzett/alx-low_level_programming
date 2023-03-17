a84ae3352d8f724952cce86947d1ef6146e4d1a5
FaneziLuzett <ftmohlahlo@gmail.com>
2023-03-16 19:55:42 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-include<stdio.h>
+#include <stdio.h>
+
 /**
- *main - Entry point
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0 when successful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece if art,\n")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece if art,");
+	return (0);
 }
