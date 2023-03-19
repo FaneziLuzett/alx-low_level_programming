1ef7a40c6d7b0b2528f80497a41f0e71163c472c
FaneziLuzett <ftmohlahlo@gmail.com>
2023-03-19 14:36:52 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'b'; i <= 'z'; i++)
+		putchar (i);
+	putchar ('\n');
+	return (0);
+}
