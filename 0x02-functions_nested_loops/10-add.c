a2fda7c49a099d1538388543b056d0fdeaab83c8
FaneziLuzett <ftmohlahlo@gmail.com>
2023-03-23 07:47:58 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,12 +4,11 @@
  * add - Add two values
  * @n1: The first number to be computed
  * @n2: The second number to be computed
+ * Return: Intern value
  */
 
 int add(int n1, int n2)
 
 {
-	int return = n1 + n2;
-
 	return (n1 + n2);
 }
